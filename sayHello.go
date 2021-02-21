62de666635dfbb0ba20b9f60c27320ee7fcb4dc0
Khilmi Aminudin <khilmiaminudin715@gmail.com>
2021-02-21 20:06:30 +0700

    add func SayHelloName

--- sayHello.go
+++ sayHello.go
@@ -1,6 +1,9 @@
 package gosayhello
 
-func SayHello() string { 
-        return "Hello SayHello"
+func SayHello() string {
+	return "Hello SayHello"
 }
 
+func SayHelloName(name string) string {
+	return "Hello " + name
+}
